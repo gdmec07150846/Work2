9adf9cdcd2c0b28bade644a5ab407a2d6fb7fc88
gdmec07150846 <1054497459@qq.com>
2016-09-18 22:38:24 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/s07150846/work2/MainActivity.java
@@ -0,0 +1,46 @@
+package cn.edu.gdmec.s07150846.work2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Work2","onStart()方法被执行");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Work2","onStop()方法被执行");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Work2","onResume()方法被执行");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Work2","onPause()方法被执行");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Work2","onRestart()方法被执行");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Work2","onDestroy()方法被执行");
+    }
+}
+
